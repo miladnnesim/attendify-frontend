e5669c428a3da5d774738533a38b3249f78c9e2d
miladnnesim <milad.nesim@student.ehb.be>
2025-05-25 15:43:55 +0200

    Update ProducerTest.php

--- volumes/tests/ProducerTest.php
+++ volumes/tests/ProducerTest.php
@@ -3,7 +3,6 @@
 
 namespace {
     require_once __DIR__ . '/../vendor/autoload.php';
-
     // --- Global stubs voor WordPress-functies en $wpdb ---
     function get_userdata($id) {
         if ($id === 999) {
@@ -55,7 +54,6 @@ namespace {
 }
 
 namespace Tests {
-    require_once __DIR__ . '/../vendor/autoload.php';
     use PHPUnit\Framework\TestCase;
     use App\Producer;
     use PhpAmqpLib\Message\AMQPMessage;
